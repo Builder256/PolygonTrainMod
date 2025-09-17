c31c23b8f66f57a5ded1a9ee6a5843e6350cb195
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-17 18:41:40 +0900

    未使用のimport文を削除

--- src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
+++ src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
@@ -2,7 +2,7 @@ package com.portofino.polygondtrainmod.block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
+//import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class GateBlock extends Block {
 //    public static final BooleanProperty OPEN = BooleanProperty.create("open");
