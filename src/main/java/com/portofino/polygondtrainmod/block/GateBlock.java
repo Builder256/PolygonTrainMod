4ba8811468e578f64e2ac85ac03905faaf7504ee
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-17 18:41:10 +0900

    改札機ブロックに下付きブロックとテストテクスチャを反映したかったが、アイテムの状態でしか反映されない

--- src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
+++ src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
@@ -2,11 +2,11 @@ package com.portofino.polygondtrainmod.block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-//import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class GateBlock extends Block {
-//    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 //    public static final BooleanProperty OPEN = BooleanProperty.create("open");
+//    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public  GateBlock(BlockBehaviour.Properties properties) {
         super(properties);
