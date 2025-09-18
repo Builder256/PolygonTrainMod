c7ac6f2a27fe3189dd056eef3caa538f55167940
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-18 21:24:31 +0900

    改札機ブロックに設置時の視点に応じた向き変更と、開いているか閉じているかのBlockStateの状態管理、それに応じたテストモデルを実装

--- src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
+++ src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
@@ -1,15 +1,37 @@
 package com.portofino.polygondtrainmod.block;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-//import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+
+import javax.annotation.Nullable;
 
 public class GateBlock extends Block {
-//    public static final BooleanProperty OPEN = BooleanProperty.create("open");
-//    public static final DirectionProperty FACING = BlockStateProperties.FACING;
+    public static final BooleanProperty OPEN = BooleanProperty.create("open");
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public GateBlock() {
         super(BlockBehaviour.Properties.of().sound(SoundType.STONE).noOcclusion());
+
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING,OPEN);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        Direction direction = pContext.getHorizontalDirection();
+        return this.defaultBlockState().setValue(FACING, direction);
     }
 }
