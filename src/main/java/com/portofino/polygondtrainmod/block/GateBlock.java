778ac60ff671778e7143d9da72f93c37181773c1
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-18 22:34:07 +0900

    向きと開閉状態に応じた当たり判定を設定

--- src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
+++ src/main/java/com/portofino/polygondtrainmod/block/GateBlock.java
@@ -1,7 +1,9 @@
 package com.portofino.polygondtrainmod.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -10,12 +12,20 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
 public class GateBlock extends Block {
     public static final BooleanProperty OPEN = BooleanProperty.create("open");
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    protected static final VoxelShape COLLISION_SHAPE_NORTH = Block.box(0.0, 0.0, 4.0, 16.0, 24.0, 6.0);
+    protected static final VoxelShape COLLISION_SHAPE_EAST = Block.box(10.0, 0.0, 0.0, 12.0, 24.0, 16.0);
+    protected static final VoxelShape COLLISION_SHAPE_SOUTH = Block.box(0.0, 0.0, 10.0, 16.0, 24.0, 12.0);
+    protected static final VoxelShape COLLISION_SHAPE_WEST = Block.box(4.0, 0.0, 0.0, 6.0, 24.0, 16.0);
 
     public GateBlock() {
         super(BlockBehaviour.Properties.of().sound(SoundType.STONE).noOcclusion());
@@ -25,7 +35,7 @@ public class GateBlock extends Block {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(FACING,OPEN);
+        pBuilder.add(FACING, OPEN);
     }
 
     @Override
@@ -33,5 +43,17 @@ public class GateBlock extends Block {
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
         Direction direction = pContext.getHorizontalDirection();
         return this.defaultBlockState().setValue(FACING, direction);
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction direction = state.getValue(FACING);
+
+        if (state.getValue(OPEN)) return Shapes.empty();
+        if (direction == Direction.NORTH) return COLLISION_SHAPE_NORTH;
+        if (direction == Direction.EAST) return COLLISION_SHAPE_EAST;
+        if (direction == Direction.SOUTH) return COLLISION_SHAPE_SOUTH;
+        if (direction == Direction.WEST) return COLLISION_SHAPE_WEST;
+        return COLLISION_SHAPE_NORTH;
     }
 }
