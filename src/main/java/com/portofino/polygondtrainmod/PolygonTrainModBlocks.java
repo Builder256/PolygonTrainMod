a96453da04fa8d12f4432eb9e0d91ddfb1df5fc4
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-18 18:54:53 +0900

    改札機に改札機かもしれない見た目を設定

--- src/main/java/com/portofino/polygondtrainmod/PolygonTrainModBlocks.java
+++ src/main/java/com/portofino/polygondtrainmod/PolygonTrainModBlocks.java
@@ -10,5 +10,5 @@ public class PolygonTrainModBlocks {
     // すべてのブロックを "polygontrainmod "名前空間に登録するために、Deferred Registerを作成する。
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PolygonTrainMod.MODID);
     // テスト改札機を登録
-    public static final DeferredBlock<GateBlock> TEST_AUTOMATIC_TICKET_GATE = BLOCKS.register("test_automatic_ticket_gate", ()-> new GateBlock(BlockBehaviour.Properties.of().sound(SoundType.STONE)));
+    public static final DeferredBlock<GateBlock> TEST_AUTOMATIC_TICKET_GATE = BLOCKS.register("test_automatic_ticket_gate", ()-> new GateBlock(BlockBehaviour.Properties.of().sound(SoundType.STONE).noOcclusion()));
 }
