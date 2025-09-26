0b6a922913ce79bc6eb32f023415706cddf8fd1a
Builder <92662457+Builder256@users.noreply.github.com>
2025-09-26 17:24:47 +0900

    券売機ブロックを作りたかった。

---
+++ src/main/java/com/portofino/polygondtrainmod/block/TicketMachineBlock.java
@@ -0,0 +1,8 @@
+package com.portofino.polygondtrainmod.block;
+
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+
+public class TicketMachineBlock extends BaseEntityBlock {
+
+}
