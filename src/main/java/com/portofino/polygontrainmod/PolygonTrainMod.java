07c518e8ab4a96c957440cbda6c3f7ea59aa7191
Builder <92662457+Builder256@users.noreply.github.com>
2026-03-14 04:49:35 +0900

    refactor: 未使用のimport分を削除

--- src/main/java/com/portofino/polygontrainmod/PolygonTrainMod.java
+++ src/main/java/com/portofino/polygontrainmod/PolygonTrainMod.java
@@ -15,7 +15,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
