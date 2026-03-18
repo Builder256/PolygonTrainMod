69c71e278363872a4c3c2cefa6b5dceffa5d6bb9
Builder <92662457+Builder256@users.noreply.github.com>
2026-03-18 17:46:53 +0900

    feat: 自動車のEntityModel登録用のコードのコメントを追加

--- src/main/java/com/portofino/polygontrainmod/PolygonTrainModClient.java
+++ src/main/java/com/portofino/polygontrainmod/PolygonTrainModClient.java
@@ -1,5 +1,6 @@
 package com.portofino.polygontrainmod;
 
+//import com.portofino.polygontrainmod.client.model.PolygonTrainModEntityRendererLayers;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -7,6 +8,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+//import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
@@ -29,4 +31,9 @@ public class PolygonTrainModClient {
         PolygonTrainMod.LOGGER.info("HELLO FROM CLIENT SETUP");
         PolygonTrainMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
+//
+//    @SubscribeEvent
+//    public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+//        PolygonTrainModEntityRendererLayers.registerLayers(event);
+//    }
 }
