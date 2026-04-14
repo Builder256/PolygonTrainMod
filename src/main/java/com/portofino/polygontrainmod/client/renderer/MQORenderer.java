e40dc81a1d424493448bc43cf0a3478aeac3e508
Builder <92662457+Builder256@users.noreply.github.com>
2026-04-14 19:45:27 +0900

    feat: 汎用的なMQOモデルの描画クラスを追加

--- src/main/java/com/portofino/polygontrainmod/client/renderer/MQORenderer.java
+++ src/main/java/com/portofino/polygontrainmod/client/renderer/MQORenderer.java
@@ -1,8 +1,16 @@
 package com.portofino.polygontrainmod.client.renderer;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.portofino.polygontrainmod.client.model.mqo.MQOModel;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import org.joml.Matrix4f;
 
 @OnlyIn(Dist.CLIENT)
-public class MQORenderer {
+public final class MQORenderer {
+    public static void render(MQOModel model, VertexConsumer buffer, Matrix4f matrix, int light) {
+
+    }
+
+
 }
