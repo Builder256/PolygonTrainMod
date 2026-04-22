670c18e16011dc683ee029c1d0aa0abdc60a393f
Builder <92662457+Builder256@users.noreply.github.com>
2026-04-22 19:05:48 +0900

    refactor: EntityRendererの実装をModClientクラスに移動

--- src/main/java/com/portofino/polygontrainmod/PolygonTrainModClient.java
+++ src/main/java/com/portofino/polygontrainmod/PolygonTrainModClient.java
@@ -1,7 +1,9 @@
 package com.portofino.polygontrainmod;
 
+import com.portofino.polygontrainmod.client.renderer.PolygonTrainModRenderers;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -18,11 +20,14 @@ import static com.portofino.polygontrainmod.PolygonTrainMod.LOGGER;
 // Modコンストラクタで登録せずとも自動的に登録されるらしい
 @EventBusSubscriber(modid = PolygonTrainMod.MODID, value = Dist.CLIENT)
 public class PolygonTrainModClient {
-    public PolygonTrainModClient(ModContainer container) {
+    public PolygonTrainModClient(IEventBus modEventBus, ModContainer container) {
         // NeoForgeがこのMODのコンフィグ画面を作成できるようにします。
         // コンフィグ画面は、Mods画面＞自分のModをクリック＞コンフィグをクリックで表示されます。
         // 設定オプションの翻訳をen_us.jsonファイルに追加することを忘れないでください。
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+
+        // Entityのレンダラを登録
+        modEventBus.addListener(PolygonTrainModRenderers::registerEntityRenderers);
     }
 
     @SubscribeEvent
